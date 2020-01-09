866a88cf7d081a9218e267fe89c110bb16f6e63d
Yasmin van Dijk <vandijk.yasmin@gmail.com>
2020-01-09 13:58:59 +0100

    added openstreetmaps map tiles

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-var mymap = L.map('map').setView([52.089204, 5.106351], 16);
+// starting location and zoom
+const startLocation = [52.089204, 5.106351];
+const startZoom = 13;
+
+// url and attribution for the map tiles (attribution is obligatory for openstreetmaps)
+const tilesUrl = 'https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png';
+const tilesAttribution = 'Tiles courtesy of <a href="http://openstreetmap.se/">OSM Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OSM</a>';
+
+// create the leaflet map and add map tiles
+const map = L.map('map').setView(startLocation, startZoom);
+
+L.tileLayer(tilesUrl, {
+    attribution: tilesAttribution,
+    maxZoom: 18
+}).addTo(map);
